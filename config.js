6614cecce193c42971f9a28e4e864aea073cab87
Mingkutee <86770955+Mingkutee@users.noreply.github.com>
2021-07-02 00:24:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Thương à!',
+    introTitle: 'Chang à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao chị cứ đi nhầm đường
-    Lạc vào tim em lẻ loi
+    Mà sao chang cứ đi nhầm đường
+    Lạc vào tim minhh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'dethuong zaa',
-    title: 'Phải chăng chị đã yêu ngay từ cái nhìn đầu tiên',
-    desc: 'Phải chăng chị đã say ngay từ chính nụ cười ấy ',
-    btnYes: 'ỏ, yêu mẩy thế :3',
-    btnNo: 'Đéo :>',
-    question:'Trên thế giới hơn 7 tỉ người mà sao chị lại yêu em <3',
-    btnReply: 'Gửi cho em <3',
-    reply: 'Thích thì yeuuu thoi <3',
-    mess: 'em biết mà 🥰. Yêu chị nhiều',
-    messDesc: 'Tối nay 8h call nhé <33',
-    btnAccept: 'Okiiiii lun <3',
+    title: 'Phải chăng chang đã yêu ngay từ cái nhìn đầu tiên',
+    desc: 'Làm người yêu minh honggg',
+    btnYes: 'Da cooooo:3',
+    btnNo: 'hong đâu,liuliu :>',
+    question:'Trên thế giới hơn 7 tỉ người mà sao chang lại yêu minh <3',
+    btnReply: 'Gửi cho minhh <3',
+    reply: 'yêu thì yêu thoiiiii <3',
+    mess: 'Minh biết mà 🥰. Yêu chang nhiều',
+    messDesc: 'sáng mai 6h gọi anh dậy nhé congchua <33',
+    btnAccept: 'davang<33',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
